043dd4ead23c5d17fa0e2426a32e6d95023bf619
Haibo Gu <ghbhaha@users.noreply.github.com>
2017-06-11 17:59:44 +0800

    Create ButtonSettings.java

--- src/org/cyanogenmod/cmparts/input/ButtonSettings.java
+++ src/org/cyanogenmod/cmparts/input/ButtonSettings.java
@@ -108,7 +108,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         SPLIT_SCREEN,
         SINGLE_HAND_LEFT,
         SINGLE_HAND_RIGHT,
-	HOME;
+        HOME;
 
         public static Action fromIntSafe(int id) {
             if (id < NOTHING.ordinal() || id > Action.values().length) {
